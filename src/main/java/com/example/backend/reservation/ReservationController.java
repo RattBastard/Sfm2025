9549d5ea67b71c705615226c35773d705630e802
Dániel Balla <balla.daniel24@gmail.com>
2025-11-23 03:13:46 +0100

    ReservationController

---
+++ src/main/java/com/example/backend/reservation/ReservationController.java
@@ -0,0 +1,25 @@
+package com.example.backend.reservation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/reservation")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public List<Reservation> allReservation() {
+        return reservationService.getAllReservation();
+    }
+}
