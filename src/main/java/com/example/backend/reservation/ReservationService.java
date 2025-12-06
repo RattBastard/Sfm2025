a7655a5ad849ed5288f638396bcc9c17aa30acf5
Dániel Balla <balla.daniel24@gmail.com>
2025-12-06 20:21:00 +0100

    ReservationService

--- src/main/java/com/example/backend/reservation/ReservationService.java
+++ src/main/java/com/example/backend/reservation/ReservationService.java
@@ -1,29 +1,18 @@
 package com.example.backend.reservation;
 
-import com.example.backend.room.Room;
-import com.example.backend.room.RoomRepository;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class ReservationService {
     @Autowired
     private  ReservationRepository reservationRepository;
-    private RoomRepository roomRepository;
 
     public List<Reservation> getAllReservation() {
         return reservationRepository.findAll();
-    }
-
-    public List<Integer> findByFloor(int floorNumber) {
-        List<Room> rooms = roomRepository.findByFloor(floorNumber);
-        return rooms.stream().map(room -> room.getRoomNumber()).collect(Collectors.toList());
     }
 
     public boolean checkIfDateAlreadyReserved(Long roomID, LocalDateTime start, LocalDateTime end) {
